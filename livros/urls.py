ff8a9f4f8ac1457c4865806186bdf1daec413fea
Let�cia Oliveira dos Santos <43705577808@>
2025-04-07 10:13:52 -0300

    task2

--- livros/urls.py
+++ livros/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('livros/', view=views.listar_livros, name='listar_livros')
+    path('livros/', view=views.listar_livros, name='listar_livros'),
+    path('api/livros/', view=views.read_livros),
 ]
--- livros/views.py
+++ livros/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from .models import Livro
+from .serializers import LivroSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 def listar_livros(request):
     livros = Livro.objects.all()
     return render(request, 'livros.html', {'livros' : livros})
+
+@api_view(['GET'])
+def read_livros(request):
+    livros = Livro.objects.all()
+    serializer = LivroSerializer(livros, many=True)
+    return Response(serializer.data)
